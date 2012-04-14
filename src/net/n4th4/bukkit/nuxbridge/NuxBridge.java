b60cc71f656a7b23d114b84b1fcac4d8860f2f0f
Théo B <munrek@gmx.com>
2012-04-14 13:55:06 +0300

    Update for bukkit #2149, and now using bPermissions plugin.

--- src/net/n4th4/bukkit/nuxbridge/NuxBridge.java
+++ src/net/n4th4/bukkit/nuxbridge/NuxBridge.java
@@ -3,31 +3,29 @@ package net.n4th4.bukkit.nuxbridge;
 import java.io.File;
 import java.util.logging.Logger;
 
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class NuxBridge extends JavaPlugin {
-    private final NBPlayerListener playerListener = new NBPlayerListener(this);
-    public Logger                  log;
-    public Configuration           config;
+
+    public Logger log;
+  
+    private FileConfiguration customConfig = null;
+    private File configFile = null;
 
     public void onEnable() {
         log = this.getServer().getLogger();
+        new NBPlayerListener(this);
 
-        File configFile = new File("plugins/NuxBridge/config.yml");
+        configFile = new File("plugins/NuxBridge/config.yml");
+        
         if (configFile.exists()) {
-            config = new Configuration(configFile);
-            config.load();
+            customConfig = YamlConfiguration.loadConfiguration(configFile);
         } else {
             log.severe("[NuxBridge] File not found : plugins/NuxBridge/config.yml");
         }
-
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.High, this);
 
         PluginDescriptionFile pdfFile = this.getDescription();
         log.info("[NuxBridge] " + pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
@@ -36,3 +34,4 @@ public class NuxBridge extends JavaPlugin {
     public void onDisable() {
     }
 }
+
